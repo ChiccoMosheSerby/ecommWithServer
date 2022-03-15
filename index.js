596acdc604790f8d99ab18f0dca52e82d021e0e6
Chicco Moshe Serby <chiccomoshe@gmail.com>
2022-03-15 21:28:36 +0200

    react router with node router

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 const bodyParser = require("body-parser");
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const products = require("./Routes/products");
 app.use("/products", products);
-app.get('/*', function (req, res) {
-  
-  res.sendFile(path.join(__dirname, '/public/index.html'), function (err) {
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"), function (err) {
     if (err) {
-  
-      res.status(500).send(err)
+      res.status(500).send(err);
     }
-  })
-})
+  });
+});
 
 let port = process.env.PORT || 2000;
 app.listen(port, function () {
