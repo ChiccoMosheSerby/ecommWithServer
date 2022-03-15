6078a4770f43b08f66d34b8edb8e0c3dc75cb18e
Chicco Moshe Serby <chiccomoshe@gmail.com>
2022-03-15 22:29:14 +0200

    no public

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 const cors = require("cors");
 app.use(cors());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const products = require("./Routes/products");
